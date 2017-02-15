08e8839facac3aac8a51eb6d159081da9a219f7d
Ming Liu <liuming@lmws.net>
2017-02-14 19:45:58 -0800

    update tests

--- spec/uri/s3_spec.rb
+++ spec/uri/s3_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe URI::S3 do
     expect(URI::S3::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  let(:uri_s3) { URI("s3://bucket/prefix") }
+  subject { uri_s3 }
+
+  it { is_expected.to be_a(URI::S3) }
+  it { expect(uri_s3.scheme).to eq('s3') }
+  it { expect(uri_s3.host).to eq('bucket') }
+  it { expect(uri_s3.path).to eq('/prefix') }
 end
